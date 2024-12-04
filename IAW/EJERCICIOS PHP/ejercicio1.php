12317cb7e1fbe56f50e4d2e1a1ee572ddc812781
Jose Javier <josejavierperera@gmail.com>
2024-12-04 13:33:09 +0100

    subida ejercicios php

---
+++ IAW/EJERCICIOS PHP/ejercicio1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1 PHP</title>
+</head>
+<body>
+    <p>
+        <p>"¿Dónde se ejecuta el código PHP: en el servidor o en el cliente?"</p>
+        <?php echo "En el servidor"; ?>
+        <p>"¿Qué tipo de contenido puede generar PHP?"</p>
+        <?php echo "Puede generar XML, HTML…."; ?>
+        <p>"¿Cuál es la diferencia entre PHP y JavaScript en términos de dónde se ejecutan?"</p>
+        <?php echo "PHP se ejecuta en el servidor y JavaScript se ejecuta en el cliente"; ?>
+    </p>
+</body>
+</html>
---
+++ IAW/EJERCICIOS PHP/ejercicio2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1 PHP</title>
+</head>
+<body>
+    <p>
+        <p>Que significa < ?php? </p>
+        <?php echo "Indica en el servidor que el codigo al que hace referencia es PHP para que sea interpretado correctamente"; ?>
+        <p>"¿Para que sirve el echo?"</p>
+        <?php echo "Sirve para que imprima el texto en la pantalla "; ?>
+        <p>"¿Que ocurre cuando ejecutas el archivo en un servidor?"</p>
+        <?php echo "Interpreta el archivo php y genera una salida en html hacia el navegador del usuario"; ?>
+    </p>
+</body>
+</html>
